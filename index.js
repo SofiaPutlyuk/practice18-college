04157b7ebf83d4b447b3c3e2c05fa1b0f76c4b6d
SofiaPutlyuk <sofiaputlyuk@gmail.com>
2026-05-26 22:12:50 +0300

    practice18

---
+++ index.js
@@ -0,0 +1,5 @@
+const wrapperStudentButton = document.getElementById("wrapperStudent")
+const searchWrapperStudent = document.querySelector(".wrapperStudent")
+wrapperStudentButton.addEventListener("click", function(){
+ searchWrapperStudent.style.background = "red"
+})
